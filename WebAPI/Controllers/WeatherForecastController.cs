535362e8fc5285abf2a35665712271aef3c45415
Klee Collage <klee.collage@gmail.com>
2024-08-16 19:28:48 -0500

    Entity Mapping
    locally managed sql database

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -4,29 +4,12 @@ namespace WebAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+// http://localhost:5000/WeatherForecasts
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+    [HttpGet]
+    public IEnumerable<string> Get() {
+        string[] nombres = new[] { "Fabiana", "Rolando", "Maria", "Juana" };
+        return nombres;
     }
 }
