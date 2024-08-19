505b9bf242340b1bf6b9dc256157d3621d125ca9
Klee Collage <klee.collage@gmail.com>
2024-08-19 13:29:43 -0500

    Handle Error Middleware

---
+++ Aplicacion/ManejadorError/ManejadorExcepcion.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Aplicacion.ManejadorError
+{
+    public class ManejadorExcepcion: Exception
+    {
+        public HttpStatusCode Codigo { get; }
+        public object Errores { get; }
+        public ManejadorExcepcion(HttpStatusCode codigo, object errores = null) {
+            Codigo = codigo;
+            Errores = errores;
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,6 +3,7 @@ using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistencia;
+using WebAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ManejadorErrorMiddleware>();
 
 app.UseHttpsRedirection();
 
