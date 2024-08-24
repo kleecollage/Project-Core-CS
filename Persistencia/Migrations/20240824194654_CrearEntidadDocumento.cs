b2d96a5e4484dadf1dc3ed82156efe089b60af7f
Klee Collage <klee.collage@gmail.com>
2024-08-24 15:24:43 -0500

    New Migration
    Entity to upload images created

---
+++ Persistencia/Migrations/20240824194654_CrearEntidadDocumento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistencia.Migrations
+{
+    public partial class CrearEntidadDocumento : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documento",
+                columns: table => new
+                {
+                    Documentoid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ObjetoReferencia = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Extension = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contenido = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documento", x => x.Documentoid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documento");
+        }
+    }
+}
